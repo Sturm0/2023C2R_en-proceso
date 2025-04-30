462160e6ec0213305cad19238e847f2c1cc0f09c
Sturm0 <FedericoGerman@outlook.es>
2025-04-30 15:55:26 -0300

    Avanzando con el parcial

--- solucion/ej1/ej1.asm
+++ solucion/ej1/ej1.asm
@@ -17,11 +17,22 @@ extern malloc
 extern free
 extern str_concat
 
-
 string_proc_list_create_asm:
-	
+	xor rdi, rdi
+	add rdi, 16
+	call malloc ; ahora en rax tengo un puntero a la memoria que acabo de reservar
+	pxor xmm0, xmm0 ; pongo un 0
+	movq [rax],xmm0    ; nuevo->first = NULL
+	movq [rax+8],xmm0  ; nuevo->last = NULL
+	ret
 
+
+; los argumentos de string_proc_node:
+; DIL <- type
+; RSI <- hash
 string_proc_node_create_asm:
+	xor rdi, rdi
+	add rdi,      ; se que ocupa 32 bytes un string_proc_node
 
 string_proc_list_add_node_asm:
 
